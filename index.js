a21e9fd023fc020d2d282f7cba498a670363e246
brug <liu.j.benjamin@gmail.com>
2025-12-14 16:43:09 -0800

    Change status to online and increase time before activity change

--- index.js
+++ index.js
@@ -144,16 +144,16 @@ client.once('ready', async () => {
 
 	client.user.setPresence({
 		activities: [activities[activityIndex]],
-		status: 'idle',
+		status: 'online',
 	});
 
 	setInterval(() => {
 		activityIndex = (activityIndex + 1) % activities.length;
 		client.user.setPresence({
 			activities: [activities[activityIndex]],
-			status: 'idle',
+			status: 'online',
 		});
-	}, 30000);
+	}, 60000);
 });
 
 // <==========>
